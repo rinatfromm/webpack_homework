fbeb7179490d4d206ee317d1601cd87a96f8ef46
Rinat Kov <frommrinat@gmail.com>
2024-10-25 22:44:05 +0200

    feat: добавить изменение фона при нажатии на картинку

--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
 import rain from "./assets/rainy-bg.jpg";
 import summer from "./assets/summer-bg.jpg";
 import winter from "./assets/winter-bg.jpg";
+import cloudRain from "./assets/icons/cloud-rain.svg";
+import cloudSnow from "./assets/icons/cloud-snow.svg";
+import sun from "./assets/icons/sun.svg";
 
 export default [
-  { id: 1, title: "Rain", img: rain },
-  { id: 2, title: "Summer", img: summer },
-  { id: 3, title: "Winter", img: winter },
+  { id: 1, title: "Rain", img: rain, icon: cloudRain, color: "#4a90e2" },
+  { id: 2, title: "Summer", img: summer, icon: cloudSnow, color: "#f9d423" },
+  { id: 3, title: "Winter", img: winter, icon: sun, color: "#8ec6e6" },
 ];
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import data from "./data";
 import "./index.scss";
 
+const body = document.querySelector("body");
 const root = document.querySelector("#app");
 
 const title = document.createElement("h1");
@@ -14,16 +15,27 @@ mainContainer.classList.add("mainContainer");
 function renderItem(item) {
   const div = document.createElement("div");
   const img = document.createElement("img");
+  const icon = document.createElement("img");
 
+  div.classList.add("imageWrapper");
   img.classList.add("imgWeather");
+  icon.classList.add("iconOverlay");
+
   img.src = item.img;
   img.alt = item.title;
 
-  div.append(img)
+  icon.src = item.icon;
+  icon.alt = `${item.title} icon`;
+
+  div.addEventListener("click", () => {
+    body.style.backgroundImage = `url(${item.img})`;
+    title.style.color = item.color;
+    console.log(`Clicked on: ${item.title}`);
+  });
+
+  div.append(img, icon);
   mainContainer.append(div);
 }
 
 data.forEach(renderItem);
-
 root.append(mainContainer);
-
