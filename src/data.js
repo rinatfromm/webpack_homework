7010010ec0f3ede1e22d19feaaebfff3905f8128
Rinat Kov <frommrinat@gmail.com>
2024-11-06 15:41:58 +0100

    Merge futureWeather into main

--- src/data.js
+++ src/data.js
@@ -9,9 +9,30 @@ import summerSound from "./assets/sounds/summer.mp3";
 import winterSound from "./assets/sounds/winter.mp3";
 
 const data = [
-  { id: 1, title: "Rain", img: rain, icon: cloudRain, color: "#4a90e2", sound: rainSound },
-  { id: 2, title: "Summer", img: summer, icon: cloudSnow, color: "#f9d423", sound: summerSound },
-  { id: 3, title: "Winter", img: winter, icon: sun, color: "#8ec6e6", sound: winterSound },
+  {
+    id: 1,
+    title: "Rain",
+    img: rain,
+    icon: cloudRain,
+    color: "#4a90e2",
+    sound: rainSound,
+  },
+  {
+    id: 2,
+    title: "Summer",
+    img: summer,
+    icon: cloudSnow,
+    color: "#f9d423",
+    sound: summerSound,
+  },
+  {
+    id: 3,
+    title: "Winter",
+    img: winter,
+    icon: sun,
+    color: "#8ec6e6",
+    sound: winterSound,
+  },
 ];
 
 export default data;
